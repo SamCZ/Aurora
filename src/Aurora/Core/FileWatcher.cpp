3c663715a65889fc06fba0e9d80055f2fc2a4304
SamCZ <rainb0wdash@seznam.cz>
2021-04-14 12:59:52 +0200

    Upgraded file watcher to support multiple system file types

--- src/Aurora/Core/FileWatcher.cpp
+++ src/Aurora/Core/FileWatcher.cpp
@@ -2,16 +2,22 @@
 
 namespace Aurora
 {
-	FileWatcher::FileWatcher(std::filesystem::path watchPath, bool recursive, std::chrono::duration<int64_t, std::milli> delay)
-	: m_WatchPath(std::move(watchPath)), m_Recursive(recursive), m_Delay(delay), m_Thread(), m_IsRunning(true), m_Paths()
+	FileWatcher::FileWatcher(std::filesystem::path watchPath, bool recursive, std::chrono::duration<int64_t, std::milli> delay, WatchFlags watchFlags)
+	: m_WatchPath(std::move(watchPath)), m_Recursive(recursive), m_Delay(delay), m_WatchFlags(watchFlags), m_Thread(), m_IsRunning(true), m_Paths()
 	{
 		m_Thread = std::thread([this]() -> void {
-			for(auto &file : std::filesystem::recursive_directory_iterator(m_WatchPath)) {
-				m_Paths[file.path()] = std::filesystem::last_write_time(file);
+			if(m_Recursive) {
+				for(auto &file : std::filesystem::recursive_directory_iterator(m_WatchPath)) {
+					if(IsFileWatchable(file)) m_Paths[file.path()] = std::filesystem::last_write_time(file);
+				}
+			} else {
+				for(auto &file : std::filesystem::directory_iterator(m_WatchPath)) {
+					if(IsFileWatchable(file)) m_Paths[file.path()] = std::filesystem::last_write_time(file);
+				}
 			}
 
 			while(m_IsRunning) {
-				Update();
+				UpdateThread();
 				std::this_thread::sleep_for(m_Delay);
 			}
 		});
@@ -30,15 +36,13 @@ namespace Aurora
 		}
 	}
 
-	void FileWatcher::Update()
+	void FileWatcher::UpdateThread()
 	{
 		auto it = m_Paths.begin();
 
 		while (it != m_Paths.end()) {
 			if (!std::filesystem::exists(it->first)) {
-				if(std::filesystem::is_regular_file(std::filesystem::path(it->first))) {
-					if(m_AsyncStatusEvent) m_AsyncStatusEvent(it->first, FileStatus::Deleted);
-				}
+				CallEvents(it->first, FileStatus::Deleted);
 				it = m_Paths.erase(it);
 			} else {
 				it++;
@@ -46,23 +50,56 @@ namespace Aurora
 		}
 
 		// Check if a file was created or modified
-		for(auto &file : std::filesystem::recursive_directory_iterator(m_WatchPath)) {
-			auto current_file_last_write_time = std::filesystem::last_write_time(file);
-
-			if(!m_Paths.contains(file.path())) {
-				m_Paths[file.path()] = current_file_last_write_time;
-
-				if(std::filesystem::is_regular_file(std::filesystem::path(file.path()))) {
-					if(m_AsyncStatusEvent) m_AsyncStatusEvent(file.path(), FileStatus::Created);
-				}
-			} else if(m_Paths[file.path()] != current_file_last_write_time) {
-				m_Paths[file.path()] = current_file_last_write_time;
-
-				if(std::filesystem::is_regular_file(std::filesystem::path(file.path()))) {
-					if(m_AsyncStatusEvent) m_AsyncStatusEvent(file.path(), FileStatus::Modified);
-				}
+		if(m_Recursive) {
+			for(auto &file : std::filesystem::recursive_directory_iterator(m_WatchPath)) {
+				UpdateFileStatus(file);
+			}
+		} else {
+			for(auto &file : std::filesystem::directory_iterator(m_WatchPath)) {
+				UpdateFileStatus(file);
 			}
 		}
+	}
+
+	void FileWatcher::UpdateFileStatus(const std::filesystem::directory_entry &entry)
+	{
+		auto current_file_last_write_time = std::filesystem::last_write_time(entry);
+
+		if(!m_Paths.contains(entry.path())) {
+			if(!IsFileWatchable(entry)) {
+				return;
+			}
+
+			m_Paths[entry.path()] = current_file_last_write_time;
+
+			CallEvents(entry.path(), FileStatus::Created);
+		} else if(m_Paths[entry.path()] != current_file_last_write_time) {
+			m_Paths[entry.path()] = current_file_last_write_time;
+
+			CallEvents(entry.path(), FileStatus::Modified);
+		}
+	}
+
+	bool FileWatcher::IsFileWatchable(const std::filesystem::path &path) const
+	{
+		if(!std::filesystem::exists(path)) {
+			return false;
+		}
+
+		if(std::filesystem::is_regular_file(path) && (m_WatchFlags & RegularFile) != 0) return true;
+		if(std::filesystem::is_directory(path) && (m_WatchFlags & Directory) != 0) return true;
+		if(std::filesystem::is_block_file(path) && (m_WatchFlags & BlockDevice) != 0) return true;
+		if(std::filesystem::is_character_file(path) && (m_WatchFlags & CharacterFile) != 0) return true;
+		if(std::filesystem::is_fifo(path) && (m_WatchFlags & IPC_Pipe) != 0) return true;
+		if(std::filesystem::is_socket(path) && (m_WatchFlags & IPC_Socket) != 0) return true;
+		if(std::filesystem::is_symlink(path) && (m_WatchFlags & Symlink) != 0) return true;
+
+		return false;
+	}
+
+	bool FileWatcher::IsFileWatchable(const std::filesystem::directory_entry& entry) const
+	{
+		return IsFileWatchable(entry.path());
 	}
 
 	std::string FileWatcher::FileStatusToString(const FileWatcher::FileStatus &fileStatus)
@@ -82,5 +119,39 @@ namespace Aurora
 	void FileWatcher::SetAsyncEventHandler(const FileWatcher::FileWatchEvent &event)
 	{
 		m_AsyncStatusEvent = event;
+	}
+
+	void FileWatcher::SetEventHandler(const FileWatcher::FileWatchEvent &event)
+	{
+		m_SyncStatusEvent = event;
+	}
+
+	void FileWatcher::Update()
+	{
+		m_Mutex.lock();
+		while(!m_SyncQueue.empty()) {
+			auto val = m_SyncQueue.front();
+			m_SyncQueue.pop();
+
+			if(m_SyncStatusEvent) m_SyncStatusEvent(val.first, val.second);
+		}
+		m_Mutex.unlock();
+	}
+
+	void FileWatcher::CallEvents(const std::filesystem::path& path, const FileStatus& status)
+	{
+		/*std::filesystem::is_regular_file(std::filesystem::path(it->first))
+		if(!path.has_extension()) {
+			return;
+		}*/
+
+
+		if(m_AsyncStatusEvent) m_AsyncStatusEvent(path, status);
+
+		if(m_SyncStatusEvent) {
+			m_Mutex.lock();
+			m_SyncQueue.push({path, status});
+			m_Mutex.unlock();
+		}
 	}
 }
