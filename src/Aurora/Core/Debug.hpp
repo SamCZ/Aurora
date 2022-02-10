236da1ef2934ec5b7ce7d7fe9e488a1d9ec09102
SamCZ <rainb0wdash@seznam.cz>
2022-02-10 21:29:37 +0100

    Fixed debug

--- src/Aurora/Core/Debug.hpp
+++ src/Aurora/Core/Debug.hpp
@@ -2,12 +2,12 @@
 
 // Define for breakpointing.
 #if defined (_WIN32)
-	#if defined __WIN32__ || defined _WIN32
+	#if defined (_MSC_VER)
+		#define ES_BREAK {__debugbreak();}
+	#elif defined __WIN32__ || defined _WIN32
 		#define ES_BREAK {asm("int $0x03");}
-	#elif defined (_MSC_VER)
-#define ES_BREAK {__debugbreak();}
-		#else
-			#define ES_BREAK
+	#else
+		#define ES_BREAK
 	#endif
 #else
 	#define ES_BREAK {__builtin_trap();}
