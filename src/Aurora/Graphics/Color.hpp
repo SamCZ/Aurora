aa82155ce05809398684c7e25e66b12eff7c6f04
SamCZ <michalconstantine@gmail.com>
2022-04-07 23:44:40 +0200

    Fixed windows build

--- src/Aurora/Graphics/Color.hpp
+++ src/Aurora/Graphics/Color.hpp
@@ -12,10 +12,10 @@ namespace Aurora
 		{
 			struct
 			{
-				uint8_t r{};
-				uint8_t g{};
-				uint8_t b{};
-				uint8_t a{};
+				uint8_t r;
+				uint8_t g;
+				uint8_t b;
+				uint8_t a;
 			};
 			uint32_t rgba;
 		};
